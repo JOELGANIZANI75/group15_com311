393de7efe25a9f1eb9e12c012ded7e65b19070ab
JOELGANIZANI75 <167243130+JOELGANIZANI75@users.noreply.github.com>
2024-11-28 02:41:35 +0200

    Update main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,16 +4,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
   app.enableCors();
- 
 
   // Increase the size limit for JSON and URL-encoded bodies
   app.use(bodyParser.json({ limit: '10mb' })); // Adjust the limit as needed
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true })); // Adjust the limit as needed
 
-  await app.listen(3001);
+  // Ensure the app listens on the port set by the environment variable (for Vercel)
+  const port = process.env.PORT || 3000; // Use Vercel's dynamic port or fallback to 3000
+  await app.listen(port);
 }
 bootstrap();
